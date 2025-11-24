8781cae77bcc17715e11d604dac5916ce7847799
Santiago <santiandermatten@gmail.com>
2025-11-24 11:36:53 -0300

    added persistence & interface

--- main.py
+++ main.py
@@ -15,26 +15,31 @@ def main():
         
         print_menu()
 
-        choice = input("Choose an option: ")
+        try: 
 
-        match choice:
-            case "1": 
-                description = input("What task do you have to do?")
-                manager.add_task(description)
-                pass
-            case "2": 
-                manager.list_tasks()
-            case "3": 
-                task_id = input("Wich task have you completed?")
-                manager.complete_task(task_id)
-            case "4": 
-                task_id = input("Wich task do you wish to delete?")
-                manager.delete_task(task_id)
-            case "5": 
-                print("Thank you for using this Task Manager...")
-                break;
-            case _: 
-                print ("Invalid Option.")
+            choice = int(input("Choose an option: "))
+
+            match choice:
+                case 1: 
+                    description = input("What task do you have to do? ")
+                    manager.add_task(description)
+                    pass
+                case 2: 
+                    manager.list_tasks()
+                case 3: 
+                    task_id = int(input("Wich task have you completed? "))
+                    manager.complete_task(task_id)
+                case 4: 
+                    task_id = input("Wich task do you wish to delete? ")
+                    manager.delete_task(task_id)
+                case 5: 
+                    print("Thank you for using this Task Manager... ")
+                    break;
+                case _: 
+                    print ("Invalid Option.")
+        except ValueError:
+            print("Invalid Option")  
+
 
 if __name__ == "__main__":
     main()
--- task_manager.py
+++ task_manager.py
@@ -1,3 +1,5 @@
+import json
+
 class Task: 
     def __init__(self, id, description, completed=False):
         self.id = id
@@ -9,15 +11,19 @@ class Task:
         return f"[{status}] #{self.id}: {self.description}"
     
 class TaskManager:
+
+    FILENAME = "task.json"
     def __init__(self):
         self._tasks = []
         self._next_id = 1
+        self.load_task()
 
     def add_task(self, description):
         task = Task(self._next_id, description)
         self._tasks.append(task)
         self._next_id += 1 
         print(f"Added {description} to the tasks list.")
+        self.save_task()
 
     def list_tasks(self):
         if not self._tasks:
@@ -30,6 +36,7 @@ class TaskManager:
         for task in self._tasks:
                 if task.id == id:
                     task.completed = True
+                    self.save_task()
                     print(f"{task.description} marked as completed.")
                     return 
         print("There is no task with that ID") 
@@ -38,6 +45,23 @@ class TaskManager:
         for task in self._tasks:
                 if task.id == id:
                     self._tasks.remove(task)
+                    self.save_task()
                     print(f"Task number #{task.id} eliminated.")
                     return 
         print("There is no task with that ID")
+
+    def load_task(self):
+        try:
+            with open(self.FILENAME, "r") as file:
+                data = json.load(file)
+                self._tasks = [Task(item["id"], item["description"], item["completed"]) for item in data]
+                if self._tasks:
+                    self._next_id = self._tasks[-1].id + 1 
+                else: 
+                    self._next_id = 1
+        except  FileNotFoundError:     
+            self._tasks = []
+    
+    def save_task(self):
+         with open(self.FILENAME, "w") as file:
+             json.dump([{"id":task.id, "description": task.description, "completed": task.completed}for task in self._tasks], file, indent=4)
